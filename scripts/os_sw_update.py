6b446968d8a435817666baa9af55c12368648f53
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-15 13:45:53 +0300

    renamed from 'update_sw.py' to 'os_sw_update.py' based on the new naming approach

---
+++ scripts/os_sw_update.py
@@ -0,0 +1,4 @@
+from modules.os_utils.sw_update import update_sw
+
+if __name__ == '__main__':
+    update_sw()
--- scripts/update_sw.py
+++
@@ -1,4 +0,0 @@
-from modules.update_sw import update_sw
-
-if __name__ == '__main__':
-    update_sw()
