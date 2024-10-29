d19d1d47c3e2da7a743d70e81bcb0d74d99bd07d
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-29 18:47:42 +0200

    refactor: update code in timer.py to be compatible with 'timer' entry point

--- ubuntu_cli/scripts/timer.py
+++ ubuntu_cli/scripts/timer.py
@@ -1,11 +1,16 @@
 from ubuntu_cli.modules.sys_paths import ubuntu_cli_dir
 from ubuntu_cli.modules.terminal import run_terminal
 
-timer: str = 'ubuntu_cli.modules.timer.run_timer'
-commands: list[str] = [
-    f'cd {ubuntu_cli_dir}',
-    f'python3 -m {timer}',
-]
+
+def run_timer() -> None:
+    timer: str = 'ubuntu_cli.modules.timer.run_timer'
+    commands: list[str] = [
+        f'cd {ubuntu_cli_dir}',
+        f'python3 -m {timer}',
+    ]
+
+    run_terminal(commands=commands, window_size='52x10')
+
 
 if __name__ == '__main__':
-    run_terminal(commands=commands, window_size='52x10')
+    run_timer()
