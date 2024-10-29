63c07ab81fe33964787389b0c3e4e5d123cbc4c2
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-10-29 18:46:03 +0200

    feat: implement 'start' entry point functionality in entry_point.py

---
+++ ubuntu_cli/modules/entry_point.py
@@ -0,0 +1,61 @@
+import os
+from subprocess import run
+
+from ubuntu_cli.modules.sys_paths import ubuntu_cli_dir
+
+
+def main() -> None:
+    available_scripts: list = get_available_scripts(
+        script_to_ignore='start_ubuntu_cli.py'
+    )
+    prompt: str = create_input_prompt(available_scripts)
+    command_input_interface(prompt, available_scripts)
+
+
+def command_input_interface(prompt: str, available_scripts: list) -> None:
+    is_active: bool = True
+
+    while is_active:
+        print(prompt)
+        command: str = input('>>> ')
+
+        if command == 'q':
+            is_active = False
+        else:
+            if command in available_scripts:
+                command: list[str] = ['poetry', 'run', f'{command}']
+                run(command, check=True)
+
+            else:
+                print(
+                    f'Command "{command}" not recognized.\n'
+                    f'Please make sure one of the commands shown in the '
+                    f'greeting is entered.'
+                )
+            print('=' * 10)
+
+
+def get_available_scripts(script_to_ignore: str) -> list:
+    available_scripts: list = os.listdir(
+        path=f'{ubuntu_cli_dir}/ubuntu_cli/scripts'
+    )
+
+    # filter out non-scripts, current file and .py extensions
+    available_scripts = [
+        os.path.splitext(script)[0]
+        for script in available_scripts
+        if script.endswith('.py') and
+           script != script_to_ignore and script != '__init__.py'
+    ]
+
+    return available_scripts
+
+
+def create_input_prompt(available_scripts: list) -> str:
+    prompt: str = (
+        'Welcome to UbuntuCLI! '
+        'Choose the script you\'d like to run by entering one of the commands '
+        'below. Alternatively, enter "q" to exit.\n' +
+        ''.join(f'\t{script}\n' for script in available_scripts)
+    )
+    return prompt
